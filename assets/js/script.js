bc5a7b7b38aa90cd9018084415ca7f7a3e5b9e49
PablitoLuquito <pablolucas-1@outlook.com>
2023-12-08 21:19:30 -0300

    update 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 let peso = document.getElementById('peso');
 let altura = document.getElementById('altura');
 let calcular = document.getElementById('calcular');
-let formImc = document.getElementById('form-imc');
+let verificacao = /[.\W]/
 
 calcular.addEventListener('click', function(event) {
   event.preventDefault();
@@ -9,10 +9,12 @@ calcular.addEventListener('click', function(event) {
 });
 
 function calcularImc() {
-  if (peso.value.length === 0 || altura.value.length === 0 || peso.value <= 0 || altura.value <= 0) {
-    return alert('[ERRO] Verifique se os dados estão corretos.\nNecessário valores acima de 0.');
+  if (peso.value.length === 0 || altura.value.length === 0 || peso.value <= 0 || altura.value <= 0 || 
+    verificacao.test(peso.value) || verificacao.test(altura.value)) {
+    return alert('[ERRO] Verifique se os dados estão corretos.\nNecessário valores acima de 0.\nNão é permitido nenhum tipo de caracter especial.');
   } else {
     let msg = document.getElementById('msg');
+    msg.classList.add('show')
     let resultado = (Number(peso.value) / ((Number(altura.value) / 100) ** 2)).toFixed(2);
     if (resultado < 18.5) {
       return msg.innerHTML = `IMC: ${resultado} = Abaixo do peso`;
